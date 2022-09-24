3226d2eb98f56e6b1b7e87480cc88e596dbd4dad
durbar chakraborty <durbardibyo@gmail.com>
2022-09-25 02:01:27 +0530

    importing necessary libraries

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 import sys
+import numpy as np
+import pytesseract
+from PIL import Image
+from cv2 import GaussianBlur, normalize, NORM_MINMAX, THRESH_BINARY, threshold
+from spellchecker import spellcheckerfrom autocorrect import spellchecker
+
